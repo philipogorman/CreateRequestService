fea6e0d296a3b36ebfe4c0eb2cc88f36571bd158
Philip O'Gorman <pogorman@koresystemsgroup.com>
2013-11-27 10:05:48 -0600

    Some cleanup
    Adding logger to TestRequestResponder, removing confusing line in
    AutoSubscriberStartable

--- CreateRequest.Subscribers/Responders/TestRequestResponder.cs
+++ CreateRequest.Subscribers/Responders/TestRequestResponder.cs
@@ -1,4 +1,5 @@
-﻿using CreateRequest.MessageTypes.Requests;
+﻿using Castle.Core.Logging;
+using CreateRequest.MessageTypes.Requests;
 using CreateRequest.MessageTypes.Responses;
 using EasyNetQ;
 
@@ -8,18 +9,29 @@ namespace CreateRequest.Subscribers.Responders
     public class TestRequestResponder : IResponder
     {
         private readonly IBus _bus;
+
+        private ILogger _logger;
+
+        public ILogger Logger
+        {
+            get { return _logger ?? (_logger = NullLogger.Instance); }
+            set { _logger = value; }
+        }
+
+
         public TestRequestResponder(IBus bus)
         {
             _bus = bus;
         }
         public void Subscribe()
         {
-            _bus.Respond<TestRequest, TestResponse>(Responce);
+            _bus.Respond<TestRequest, TestResponse>(Response);
         
         }
 
-        private TestResponse Responce(TestRequest request)
+        private TestResponse Response(TestRequest request)
         {
+            Logger.InfoFormat("Received request: {0}", request.Request);
            return new TestResponse {Response = " ***** Response to Request *****"};
         }
     }
